c66c43b3146d0ba8fafe384dc89b4120b3b53547
Artur Babayan <Artur.Babayan@acba.am>
2022-05-18 19:51:24 +0400

    [add] quadratic

--- sripts/indexQP.js
+++ sripts/indexQP.js
@@ -3,9 +3,8 @@ var createObjectFunction = function () {
     var count = document.getElementById("variable_count").value;
     var p_count = document.getElementById("parameter_count").value;
     var p = "";
-    for (var i = 0; i < count; i++) {
-        p += ("+(<input class='coefficient' id='c_" + (i + 1) + "'" + "type='text'/>)*x<sub>" + (i + 1) + "</sub>");
-    }
+    p += ("<input class='coefficientQP' id='c_0' type='text'/>");
+    
     p += "-><select id='extremum'><option value='max'>Max</option><option value='min'>Min</option></select>";
     document.getElementById("object").innerHTML = p;
     parameters = "";
@@ -15,6 +14,7 @@ var createObjectFunction = function () {
     parameters = '<div class="grid-container"><div class="grid-item"></div><div class="grid-item">Պարամետրի անվանումը</div><div class="grid-item">Սկզբնական մոտարկման կենտրոնը</div><div class="grid-item">Սահմանը</div>'
         + parameters + '</div>';
     document.getElementById("parameter_values").innerHTML = parameters;
+
     var solution = document.getElementById("solution");
     solution.classList.remove("hidden");
 }
@@ -22,9 +22,7 @@ var createObjectFunction = function () {
 var addConstraint = function () {
     var count = document.getElementById("variable_count").value;
     var p = "";
-    for (var i = 0; i < count; i++) {
-        p += ("+(<input class='coefficient' id='a_" + (row + 1) + "_" + (i + 1) + "'" + " type='text'/>)*x<sub>" + (i + 1) + "</sub>");
-    }
+    p += ("<span>"+ (row + 1) +")    </span><input class='coefficientQP' id='a_" + (row + 1) + "type='text'/>");
     p = "<p>" + p + "<select><option value='equal'>=</option><option value='gequal'>>=</option><option value='sequal'><=</option></select><input class='coefficient' id='b_" + (row + 1) + " type='text'/></p>";
     row++;
     document.getElementById("constraint").innerHTML += p;
